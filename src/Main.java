417f8ef77e515cbac6bf5d80dd17242bb608300a
MarinaNikolaieva <60624855+MarinaNikolaieva@users.noreply.github.com>
2021-12-11 13:15:00 +0200

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,5 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-package PARCSJPack;
+package tempor;
 
-/**
- *
- * @author Marina Nik
- */
 import parcs.*;
 import java.util.*;
 import java.io.*;
